b553e57b6941c8aa01c131fbbbd44640a225d7c2
Alejandro Endo <theHilikus@gmail.com>
2012-11-10 23:49:39 -0500

    -Added dependency for bytecode analysis -Changed some default settings to allow testing

--- src/main/java/ca/hilikus/jrobocom/GameSettings.java
+++ src/main/java/ca/hilikus/jrobocom/GameSettings.java
@@ -25,7 +25,7 @@ public class GameSettings {
 	public static final int ADVANCED_SET_PENALTY = 0;
 	public static final int SUPER_SET_PENALTY = 0;
 	public static final int MOVE = 0;
-	public static final int BANK_CHANGE = 0;
+	public static final int BANK_CHANGE = 2;
 	public static final int SCAN_BASE = 0;
 	public static final int SCAN_PER_DIST = 0;
 	public static final int TURN = 0;
@@ -33,9 +33,9 @@ public class GameSettings {
 
     public static final int MAX_CREATE_WAIT = 0;
     public static final int MAX_BANKS = 50;
-    public static final int MAX_GENERATION = 0;
+    public static final int MAX_GENERATION = 15;
     public static final int BOARD_SIZE = 0;
-    public static final int MAX_AGE = 0;
+    public static final int MAX_AGE = 1000;
     public static final int MAX_BOTS = 0;
     public static final int MAX_WORLD_AGE = 0;
 
