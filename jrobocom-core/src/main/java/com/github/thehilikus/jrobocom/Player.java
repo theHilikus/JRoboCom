28d4b730832993ea6e38ff37a4f5d8959640a8d1
Alejandro Endo <theHilikus@gmail.com>
2013-11-07 00:34:38 -0500

    -Refactored constructor a bit to facilitate unit testing -Added check for empty banks list -Added check for inexistent bank -Created unit tests for Player.java

--- jrobocom-core/src/main/java/com/github/thehilikus/jrobocom/Player.java
+++ jrobocom-core/src/main/java/com/github/thehilikus/jrobocom/Player.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.security.SecureClassLoader;
@@ -31,7 +32,7 @@ import com.github.thehilikus.jrobocom.robot.Robot;
  */
 public class Player {
 
-    private static final int DEFAULT_START_STATE = 1;
+    static final int DEFAULT_START_STATE = 1;
 
     private static final String PLAYER_PROPERTIES_FILE = System.getProperty("jrobocom.player-properties",
 	    "player.properties");
@@ -62,22 +63,20 @@ public class Player {
     private URLClassLoader loader;
 
     /**
-     * @param codePath path to player's code. If ends in '/' it assumes the code is in .class'es in
-     *            a directory; otherwise assumes a jar
-     * @throws PlayerException if there is a problem loading the code
+     * Internal constructor
+     * 
+     * @param pLoader the loader used to import the code into the game
+     * @param codePath file system path of the location to load from
+     * @throws PlayerException if there's an exception loading the player's code
      */
-    public Player(File codePath) throws PlayerException {
-	if (!codePath.exists() || !codePath.canRead()) {
-	    throw new IllegalArgumentException("Path is invalid: " + codePath
-		    + ". It can't be read or it doesn't exist");
-	}
+    Player(URLClassLoader pLoader, String codePath) throws PlayerException {
+	loader = pLoader;
 
 	if (Thread.currentThread().getPriority() < ROBOTS_MAX_PRIORITY) {
 	    throw new PlayerException("Robot priority cannot be greater than game's");
 	}
 
 	try {
-	    loader = new URLClassLoader(new URL[] { codePath.toURI().toURL() });
 	    InputStream stream = loader.getResourceAsStream(PLAYER_PROPERTIES_FILE);
 
 	    if (stream == null) {
@@ -89,7 +88,7 @@ public class Player {
 		playerInfo.load(stream);
 
 		// read parameters
-		author = playerInfo.getProperty("Author", "Unknown programmer");
+		author = playerInfo.getProperty("Author", "Unknown developer");
 
 		teamName = playerInfo.getProperty("Team", "Unknown team");
 
@@ -98,7 +97,7 @@ public class Player {
 
 		String banksList = playerInfo.getProperty("Banks");
 
-		if (banksList == null) {
+		if (banksList == null || banksList.isEmpty()) {
 		    throw new PlayerException("Error loading configuration property: No banks definition found");
 		}
 		String[] banksClasses = banksList.split(",");
@@ -120,7 +119,30 @@ public class Player {
 	    closeClassLoader();
 	    throw exc;
 	}
+    }
 
+    /**
+     * Main constructor
+     * 
+     * @param codePath path to player's code. If ends in '/' it assumes the code is in .class'es in
+     *            a directory; otherwise assumes a jar
+     * @throws PlayerException if there is a problem loading the code
+     */
+    public Player(File codePath) throws PlayerException {
+	this(classLoaderCreator(codePath), codePath.getAbsolutePath());
+    }
+
+    private static URLClassLoader classLoaderCreator(File codePath) throws PlayerException {
+	if (!codePath.exists() || !codePath.canRead()) {
+	    throw new IllegalArgumentException("Path is invalid: " + codePath
+		    + ". It can't be read or it doesn't exist");
+	}
+
+	try {
+	    return new URLClassLoader(new URL[] { codePath.toURI().toURL() });
+	} catch (MalformedURLException exc) {
+	    throw new PlayerException("Error loading player's code", exc);
+	}
     }
 
     private static int getNextTeamId() {
@@ -144,7 +166,11 @@ public class Player {
 
 	Bank[] playerBanks = new Bank[banksClasses.length];
 	for (int pos = 0; pos < playerBanks.length; pos++) {
-	    Class<? extends Bank> bankClass = (Class<? extends Bank>) pLoader.loadClass(banksClasses[pos].trim());
+	    String className = banksClasses[pos].trim();
+	    Class<? extends Bank> bankClass = (Class<? extends Bank>) pLoader.loadClass(className);
+	    if (bankClass == null) {
+		throw new PlayerException("Bank class \"" + className + "\" was not found in jar");
+	    }
 	    try {
 		playerBanks[pos] = bankClass.getDeclaredConstructor().newInstance();
 		playerBanks[pos].setTeamId(teamId);
@@ -198,6 +224,9 @@ public class Player {
     public static List<Player> loadPlayers(List<String> playersFiles) throws PlayerException {
 	log.info("[loadPlayers] Loading all players");
 	List<Player> players = new ArrayList<>();
+	if (playersFiles.size() < 1) {
+	    log.warn("[loadPlayers] No players to load");
+	}
 
 	for (String singlePath : playersFiles) {
 	    Player single = new Player(new File(singlePath));
---
+++ jrobocom-core/src/test/java/com/github/thehilikus/jrobocom/PlayerTest.java
@@ -0,0 +1,153 @@
+package com.github.thehilikus.jrobocom;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.StringBufferInputStream;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+import com.github.thehilikus.jrobocom.exceptions.BankInterruptedException;
+import com.github.thehilikus.jrobocom.exceptions.PlayerException;
+import com.github.thehilikus.jrobocom.player.Bank;
+import com.github.thehilikus.jrobocom.robot.Robot;
+import com.github.thehilikus.jrobocom.robot.api.RobotStatusLocal;
+
+/**
+ * Tests for {@link Player}
+ * 
+ * @author hilikus
+ */
+public class PlayerTest {
+    private Player TU;
+    private URLClassLoader loader;
+    private static final Logger log = LoggerFactory.getLogger(PlayerTest.class);
+
+    private static class DummyBank extends Bank {
+	@SuppressWarnings("unused")
+	public DummyBank() {
+
+	}
+
+	@Override
+	public void run() throws BankInterruptedException {
+	    // TODO Auto-generated method stub
+
+	}
+
+    }
+
+    /**
+     * Configures the common fixtures
+     * 
+     * @throws PlayerException
+     */
+    @BeforeMethod
+    public void beforeMethod() throws PlayerException {
+	loader = mock(URLClassLoader.class);
+
+    }
+
+    private void simulatePropertiesFile(String content) throws PlayerException {
+	InputStream value = null;
+	try {
+	    value = new ByteArrayInputStream(content.getBytes("UTF-8"));
+	} catch (UnsupportedEncodingException exc) {
+	    log.error("[setPropertiesFile]", exc);
+	}
+	when(loader.getResourceAsStream(anyString())).thenReturn(value);
+	TU = new Player(loader, "unit/test/path");
+    }
+
+    /**
+     * Tests when the property file has no "banks" entry
+     * 
+     * @throws PlayerException
+     */
+    @Test(expectedExceptions = PlayerException.class)
+    public void constructNoBanks() throws PlayerException {
+	simulatePropertiesFile("Author=unit-tester");
+    }
+
+    /**
+     * Tests when the property file has an empty "banks" entry
+     * 
+     * @throws PlayerException
+     */
+    @Test(expectedExceptions = PlayerException.class)
+    public void constructEmptyBanks() throws PlayerException {
+	simulatePropertiesFile("Author=unit-tester\nBanks=");
+    }
+
+    /**
+     * Tests when the bank referenced does not exist
+     * 
+     * @throws PlayerException
+     */
+    @Test(expectedExceptions = PlayerException.class)
+    public void constructMissingBanks() throws PlayerException {
+	simulatePropertiesFile("Author=unit-tester\nBanks=crazy.class.Bank");
+    }
+
+    /**
+     * Tests building a good player with dummy banks
+     * 
+     * @throws ClassNotFoundException
+     * @throws PlayerException
+     */
+    @Test
+    public void constructGoodBanks() throws ClassNotFoundException, PlayerException {
+	doReturn(DummyBank.class).when(loader).loadClass("com.github.thehilikus.jrobocom.PlayerTest.DummyBank");
+	final String AUTHOR = "unit-tester";
+	final String TEAM = "the_testers";
+	simulatePropertiesFile("Author=" + AUTHOR + "\nBanks=com.github.thehilikus.jrobocom.PlayerTest.DummyBank\n"
+		+ "Team=" + TEAM);
+	assertEquals(TU.getAuthor(), AUTHOR, "Author's name was not loaded");
+	assertEquals(TU.getTeamName(), TEAM, "Team's name was not loaded");
+
+	// validate banks
+	assertEquals(TU.getCode().length, 1, "Wrong number of banks loaded");
+	assertEquals(TU.getCode()[0].getClass(), DummyBank.class);
+    }
+
+    /**
+     * Tests loading players from paths that don't have anything
+     * 
+     * @throws PlayerException
+     */
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void loadInexistentPlayers() throws PlayerException {
+	List<String> paths = new ArrayList<>();
+	paths.add("whatever/foo");
+	Player.loadPlayers(paths);
+    }
+
+    /**
+     * Tests whether starting a robot notifies the robot about its change in state
+     */
+    @Test
+    public void startRobot() {
+	Robot mockRobot = mock(Robot.class);
+	RobotStatusLocal mockData = mock(RobotStatusLocal.class);
+	when(mockRobot.getData()).thenReturn(mockData);
+	TU.startRobot(mockRobot);
+	verify(mockData).setActiveState(Player.DEFAULT_START_STATE);
+    }
+}
